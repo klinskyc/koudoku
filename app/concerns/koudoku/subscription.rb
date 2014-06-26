36f0a626067a09ce5596d49c0619aff847f75c74
William Porter <dawilster143@gmail.com>
2014-06-26 13:58:59 +1000

    cancelling a subscription keeps the plan active until the end of the billing period

--- app/concerns/koudoku/subscription.rb
+++ app/concerns/koudoku/subscription.rb
@@ -34,7 +34,7 @@ module Koudoku::Subscription
             prepare_for_upgrade if upgrading?
 
             # update the package level with stripe.
-            customer.update_subscription(:plan => self.plan.stripe_id)
+            customer.update_subscription(:plan => self.plan.stripe_id, :at_period_end =>true)
 
             finalize_downgrade! if downgrading?
             finalize_upgrade! if upgrading?
