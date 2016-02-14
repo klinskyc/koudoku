0bedd889c7ce1c7263ae6c61ac21d7dd3979b02c
Cavan Klinsky <cavan@cjkwebdesign.com>
2016-02-13 20:18:35 -0500

    trying

--- app/concerns/koudoku/subscription.rb
+++ app/concerns/koudoku/subscription.rb
@@ -75,7 +75,7 @@ module Koudoku::Subscription
               }
 
               # If the class we're being included in supports coupons ..
-              if self.class.name == "User"
+              if self.class.name == "Subscription" || self.class.name == "User"
                 dddd
                 if @coupon.present?
                   customer_attributes[:coupon] = @coupon.code
